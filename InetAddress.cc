3e6bbb970767e9fdc4eac186d2c1fc9914ba7b28
Pang-GJ <2362421192@qq.com>
2022-07-29 17:22:59 +0800

    add InetAddress class

---
+++ InetAddress.cc
@@ -0,0 +1,40 @@
+#include "InetAddress.h"
+#include <sys/socket.h>
+
+#include <cstring>
+
+InetAddress::InetAddress(uint16_t port, const std::string &ipv4) {
+  bzero(&addr_, sizeof addr_);
+  addr_.sin_family = AF_INET;
+  addr_.sin_port = htons(port);
+  addr_.sin_addr.s_addr = inet_addr(ipv4.c_str());
+}
+
+std::string InetAddress::toIp() const {
+  char buf[64] = {0};
+  ::inet_ntop(AF_INET, &addr_.sin_addr, buf, sizeof buf);
+  return buf;
+}
+
+std::string InetAddress::toIpPort() const {
+  char buf[64] = {0};
+  ::inet_ntop(AF_INET, &addr_.sin_addr, buf, sizeof buf);
+
+  size_t end = strlen(buf);
+  uint16_t port = ntohs(addr_.sin_port);
+
+  sprintf(buf + end, ":%u", port);
+  return buf;
+}
+
+uint16_t InetAddress::toPort() const { return ntohs(addr_.sin_port); }
+
+// 测试InetAddress
+//
+// #include <iostream>
+//
+// int main(int argc, char *argv[]) {
+//   InetAddress addr(8080);
+//   std::cout << addr.toIpPort() << std::endl;
+//   return 0;
+// }
---
+++ InetAddress.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <arpa/inet.h>
+#include <string>
+
+// 封装socket地址类型
+class InetAddress {
+ public:
+  explicit InetAddress(uint16_t port, const std::string &ipv4 = "127.0.0.1");
+
+  explicit InetAddress(const struct sockaddr_in &addr) : addr_(addr) {}
+
+  std::string toIp() const;
+  std::string toIpPort() const;
+  uint16_t toPort() const;
+
+  const sockaddr_in *getSockAddr() const { return &addr_; }
+
+ private:
+  struct sockaddr_in addr_;
+};
