f194b956afd5fcf2c33023d4c6315769ab577f4f
Pang-GJ <2362421192@qq.com>
2022-07-29 16:06:33 +0800

    add noncopyable

---
+++ noncopyable.h
@@ -0,0 +1,15 @@
+#pragma once
+
+/**
+ * noncopyable 被继承以后，派生类对象可以正常地构造和析构，但
+ * 派生类对象无法进行拷贝构造和析构
+ */
+class noncopyable {
+ public:
+  noncopyable(const noncopyable &) = delete;
+  noncopyable &operator=(const noncopyable &) = delete;
+
+ protected:
+  noncopyable() = default;
+  ~noncopyable() = default;
+};
