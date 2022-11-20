1df70936de49351101809bfeff2f2917d5dab125
Pang-GJ <2362421192@qq.com>
2022-11-20 10:56:04 +0800

    test: add http_server test

--- tests/net/test_http.cpp
+++ tests/net/test_http.cpp
@@ -1,4 +1,5 @@
 #include "common/logger.hpp"
+#include "common/json.hpp"
 #include "net/http_all.hpp"
 #include "net/tcp_all.hpp"
 
@@ -10,18 +11,35 @@ int main(int argc, char *argv[]) {
   http_app.RegisterHandle("GET", "/",
                           [](const net::http::RequestPtr &request,
                              const net::http::ResponsePtr &response) {
-                            LOG_INFO("Found a GET request");
                             response->SetHeader("Content-Type", "text/html");
                             response->SetStatusCode(200);
                             response->SetBody("<html>Hello</html>");
                           });
 
-  http_app.RegisterHandle("POST", "/", [](const net::http::RequestPtr &request, const net::http::ResponsePtr &response) {
-    LOG_INFO("Found a POST request");
+  http_app.RegisterHandle("POST", "/",
+                          [](const net::http::RequestPtr &request,
+                             const net::http::ResponsePtr &response) {
+                            LOG_INFO("POST run: %s", request->body_.c_str());
+                            response->SetHeader("Content-Type", "text/html");
+                            response->SetStatusCode(200);
+                            response->SetBody("Hello");
+                          });
+
+  http_app.POST("/add/", [](const net::http::RequestPtr &request,
+                           const net::http::ResponsePtr &response) {
     LOG_INFO("POST run: %s", request->body_.c_str());
     response->SetHeader("Content-Type", "text/html");
+    if (request->body_params_.size() < 2) {
+      LOG_INFO("body_param size: %lu", request->body_params_.size());
+      response->SetStatusCode(404);
+      response->SetBody("Error Param");
+      return;
+    }
     response->SetStatusCode(200);
-    response->SetBody("Hello");
+    auto param1 = atol(request->body_params_["param1"].c_str());
+    auto param2 = atol(request->body_params_["param2"].c_str());
+    auto res = param1 + param2;
+    response->SetBody(std::to_string(res));
   });
 
   net::TcpServer server(addr, thread_pool, &http_app);
