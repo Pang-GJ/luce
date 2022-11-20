eb1afa1b2d9f23649fc0b0cf84e8cc8b00095793
Pang-GJ <2362421192@qq.com>
2022-11-20 15:37:48 +0800

    tests: fix http-server test

--- tests/net/test_http.cpp
+++ tests/net/test_http.cpp
@@ -1,5 +1,6 @@
-#include "common/logger.hpp"
+#include <string>
 #include "common/json.hpp"
+#include "common/logger.hpp"
 #include "net/http_all.hpp"
 #include "net/tcp_all.hpp"
 
@@ -8,38 +9,17 @@ int main(int argc, char *argv[]) {
   net::InetAddress addr{12345};
   net::http::HttpServer http_app;
 
-  http_app.RegisterHandle("GET", "/",
-                          [](const net::http::RequestPtr &request,
-                             const net::http::ResponsePtr &response) {
-                            response->SetHeader("Content-Type", "text/html");
-                            response->SetStatusCode(200);
-                            response->SetBody("<html>Hello</html>");
-                          });
+  http_app.POST("/add/", [](const net::http::ContextPtr &ctx) {
+    LOG_INFO("POST run: %s", ctx->req_->body_.c_str());
 
-  http_app.RegisterHandle("POST", "/",
-                          [](const net::http::RequestPtr &request,
-                             const net::http::ResponsePtr &response) {
-                            LOG_INFO("POST run: %s", request->body_.c_str());
-                            response->SetHeader("Content-Type", "text/html");
-                            response->SetStatusCode(200);
-                            response->SetBody("Hello");
-                          });
-
-  http_app.POST("/add/", [](const net::http::RequestPtr &request,
-                           const net::http::ResponsePtr &response) {
-    LOG_INFO("POST run: %s", request->body_.c_str());
-    response->SetHeader("Content-Type", "text/html");
-    if (request->body_params_.size() < 2) {
-      LOG_INFO("body_param size: %lu", request->body_params_.size());
-      response->SetStatusCode(404);
-      response->SetBody("Error Param");
+    auto param1 = ctx->QueryBody("param1");
+    auto param2 = ctx->QueryBody("param2");
+    if (param1.empty() || param2.empty()) {
+      ctx->HTML(404, "Error Param");
       return;
     }
-    response->SetStatusCode(200);
-    auto param1 = atol(request->body_params_["param1"].c_str());
-    auto param2 = atol(request->body_params_["param2"].c_str());
-    auto res = param1 + param2;
-    response->SetBody(std::to_string(res));
+    auto res = atoi(param1.c_str()) + atoi(param2.c_str());
+    ctx->HTML(200, Format("res: {}\n", res));
   });
 
   net::TcpServer server(addr, thread_pool, &http_app);
