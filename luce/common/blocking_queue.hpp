3d13dccccbb6c93ed7ffb090ad2808c774ced883
Pang-GJ <2362421192@qq.com>
2023-02-28 08:41:48 +0800

    feat(common): update blocking queue

--- luce/common/blocking_queue.hpp
+++ luce/common/blocking_queue.hpp
@@ -1,53 +1,79 @@
 #pragma once
 
 #include <condition_variable>
+#include <cstddef>
 #include <mutex>
-#include <optional>
 #include <queue>
-
-#include "luce/common/logger.hpp"
 
 template <typename T>
 class BlockingQueue {
  public:
-  void push(const T &value) {
-    std::lock_guard<std::mutex> lock(mtx_);
-
-    queue_.push(value);
-    cv_.notify_one();
+  void push(T &&value) {
+    {
+      std::scoped_lock<std::mutex> lock(mtx_);
+      queue_.push(std::move(value));
+    }
+    cond_.notify_one();
   }
 
-  auto pop() -> std::optional<T> {
+  bool try_push(const T &value) {
+    {
+      std::unique_lock<std::mutex> lock(mtx_, std::try_to_lock);
+      if (!lock) {
+        return false;
+      }
+      queue_.push(value);
+    }
+    cond_.notify_one();
+    return true;
+  }
+
+  bool pop(T &item) {
     std::unique_lock<std::mutex> lock(mtx_);
-    cv_.wait(lock,
-             [this]() { return this->closed_.test() || !queue_.empty(); });
-
-    if (closed_.test()) {
-      return {};
-    }
+    cond_.wait(lock, [&]() { return !queue_.empty() || stop_; });
     if (queue_.empty()) {
-      LOG_ERROR("queue empty");
+      return false;
     }
-
-    T value = queue_.front();
+    item = std::move(queue_.front());
     queue_.pop();
-    return value;
+    return true;
   }
 
-  auto size() { return queue_.size(); }
+  // non-blocking pop an item, maybe failed
+  bool try_pop_if(T &item, bool (*predict)(T &) = nullptr) {
+    std::unique_lock<std::mutex> lock(mtx_, std::try_to_lock);
+    if (!lock || queue_.empty()) {
+      return false;
+    }
 
-  auto destroy() -> std::queue<T> & {
-    std::lock_guard<std::mutex> lock(mtx_);
-    closed_.test_and_set();
-    cv_.notify_all();
+    if (predict && !predict(queue_.front())) {
+      return false;
+    }
 
-    return queue_;
+    item = std::move(queue_.front());
+    queue_.pop();
+    return true;
+  }
+
+  std::size_t size() const {
+    std::scoped_lock<std::mutex> lock(mtx_);
+    return queue_.size();
+  }
+
+  bool empty() const {
+    std::scoped_lock<std::mutex> lock(mtx_);
+    return queue_.empty();
+  }
+
+  void stop() {
+    stop_ = false;
+    cond_.notify_all();
   }
 
  private:
-  std::mutex mtx_;
-  std::condition_variable cv_;
+  mutable std::mutex mtx_;
+  std::condition_variable cond_;
   std::queue<T> queue_;
 
-  std::atomic_flag closed_;
+  std::atomic<bool> stop_{false};
 };
